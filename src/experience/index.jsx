b7e4a4eaeb2c88460ce97ce6e9267bb005920a73
Ghayas <ghayasleo99@gmail.com>
2023-01-09 02:10:04 +0500

    Added leva debug panel and performance meter

--- src/experience/index.jsx
+++ src/experience/index.jsx
@@ -1,65 +1,73 @@
-import {
-  Float,
-  Html,
-  MeshReflectorMaterial,
-  OrbitControls,
-  PivotControls,
-  Text,
-  TransformControls,
-} from "@react-three/drei";
 import React, { useRef } from "react";
+import * as THREE from "three";
+import { OrbitControls, useHelper } from "@react-three/drei";
+import { Perf } from "r3f-perf";
+import { useControls } from "leva";
 
 const Experience = () => {
-  const sphere = useRef();
+  // refs
   const cube = useRef();
+  const sphere = useRef();
+  const directionalLight = useRef();
+
+  useHelper(directionalLight, THREE.DirectionalLightHelper);
+
+  // debug
+  const { Debug: enableDebug } = useControls("Debug panel", {
+    Debug: true,
+  });
+  const { position } = useControls("Transform controls", {
+    position: {
+      value: { x: 2, y: 0 },
+      step: 0.01,
+      joystick: "invertY",
+    },
+  });
 
   return (
     <>
+      {enableDebug && <Perf position="top-left" />}
       <OrbitControls makeDefault maxPolarAngle={Math.PI / 2 - 0.1} />
-
-      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <directionalLight
+        ref={directionalLight}
+        castShadow
+        position={[1, 2, 3]}
+        intensity={1.5}
+        shadow-mapSize={[4096, 4096]}
+        shadow-camera-near={1}
+        shadow-camera-far={10}
+        shadow-camera-top={5}
+        shadow-camera-right={5}
+        shadow-camera-bottom={-5}
+        shadow-camera-left={-5}
+      />
       <ambientLight intensity={0.5} />
 
-      <PivotControls anchor={[0, 0, 0]} depthTest={false} lineWidth={3}>
-        <mesh ref={cube} position-x={2} scale={1.5}>
-          <boxGeometry />
-          <meshStandardMaterial color="mediumpurple" />
-          <Html
-            wrapperClass="label"
-            center
-            distanceFactor={8}
-            position={[0, 0, 0.51]}
-            occlude={[sphere, cube]}>
-            Hello world
-          </Html>
-        </mesh>
-      </PivotControls>
-
-      <mesh ref={sphere} position-x={-2}>
+      <mesh
+        ref={cube}
+        castShadow
+        position={[position.x, position.y, 0]}
+        scale={1.5}>
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+      <mesh ref={sphere} castShadow position-x={-2}>
         <sphereGeometry />
         <meshStandardMaterial color="orange" />
       </mesh>
-
-      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+      <mesh
+        position-y={-1}
+        receiveShadow
+        rotation-x={-Math.PI * 0.5}
+        scale={10}>
         <planeGeometry />
-        <MeshReflectorMaterial
+        <meshStandardMaterial
           resolution={720}
           blur={[1000, 1000]}
           mixBlur={1}
-          color="greenyellow"
+          color="yellow"
         />
       </mesh>
-
-      <Float></Float>
-      <Text
-        position={[0, 2, -1]}
-        fontSize={1}
-        font="./bangers-v20-latin-regular.woff"
-        color="salmon">
-        I love R3F
-      </Text>
-
-      <TransformControls object={sphere} mode="translate" />
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from "@react-three/fiber"
 import Experience from "./experience"
+import { StrictMode } from "react"
+import { Leva } from "leva"
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
-
 root.render(
-  <Canvas camera={{ fov: 45, position: [- 4, 3, 6] }}>
-    <Experience />
-  </Canvas>
+  <StrictMode>
+    <Leva />
+    <Canvas shadows={false} camera={{ fov: 45, position: [- 4, 3, 6] }}>
+      <color args={["#111111"]} attach="background"/>
+      <Experience />
+    </Canvas>
+  </StrictMode>
 )
